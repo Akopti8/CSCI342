95dd07b8613b29024269e2ffca3456c493465094
Anton M. Kopti <54958829+Akopti8@users.noreply.github.com>
2022-04-28 19:16:57 -0400

    new line character

--- CSCI342/lab1/greeting.c
+++ CSCI342/lab1/greeting.c
@@ -4,8 +4,8 @@ int main(int argc, char *argv[]) {
         if(argc ==2){
                 printf("Hello %s\n",argv[1]);
         }else if(argc >2){
-                printf("too many arguments");
+                printf("too many arguments\n");
         }else{
-                printf("too little arguments");
+                printf("too little arguments\n");
         }
 }
